a5fc66ce3b384ca9ff2436dc325c5e925e43fd2e
zhangirisz <zhangirisz@gmail.com>
2019-11-24 15:51:33 -0600

    Added shop screen

--- Shop.c
+++ Shop.c
@@ -19,6 +19,31 @@
 //	player->playerInventory[item.index].count++;
 //}
 
+	/// SHOP STUFF
+//	SpriteType bobaSprite = {boba, 40, 40};
+//	SpriteType potionSprite = {healthPotion, 40, 40};
+//	SpriteType pokeballSprite = {pokeball, 40, 40};
+//	
+//	ItemType bobaItem = {"boba", bobaSprite, 60, "60 C", 2};
+//	ItemType potionItem = {"health potion", potionSprite, 30, "30 C", 1};
+//	ItemType pokeballItem = {"pokeball", pokeballSprite, 20, "20 C", 0};
+//	
+//	ItemInstType bobaShop = {86, 80, bobaItem};
+//	ItemInstType potionShop = {44, 80, potionItem};
+//	ItemInstType pokeballShop = {2, 80, pokeballItem};
+//	
+//	const ItemInstType shopItems[] = {pokeballShop, potionShop, bobaShop};
+//	
+//	const SpriteInstType itemInsts[3] = {
+//		(SpriteInstType) {pokeballShop.xPos, pokeballShop.yPos+pokeballShop.item.sprite.height-1, pokeballShop.item.sprite},
+//		(SpriteInstType) {potionShop.xPos, potionShop.yPos+potionShop.item.sprite.height-1, potionShop.item.sprite},
+//		(SpriteInstType) {bobaShop.xPos, bobaShop.yPos+bobaShop.item.sprite.height-1, bobaShop.item.sprite}
+//	};
+//	SpriteSelectType shopScreen = {itemInsts, 3, 0};
+	// DrawShopScreen(shopScreen, shopItems);
+	
+	// END OF SHOP STUFF
+
 void DrawShopScreen(SpriteSelectType shopScreen, const ItemInstType shopItems[3]){
 	ST7735_FillScreen(0xFFFF);
 	
