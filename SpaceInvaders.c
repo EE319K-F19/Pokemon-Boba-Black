3482abff61f43821a8f1afbf60b21d7a47e18a52
zhangirisz <zhangirisz@gmail.com>
2019-12-04 12:46:41 -0600

    Fixed some spanish bugs

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -126,11 +126,11 @@ void PrintWinLoseScreen(bool win, uint8_t language){
 	ST7735_SetTextColor(ST7735_BLACK);
 	ST7735_SetCursor(1, 8);
 	if(win) {
-		if(language) ST7735_OutString("\xADTu personaje\n es muy feliz!\n \xADT\xA3 ganas!");
+		if(language) ST7735_OutString("Tu personaje\n es muy feliz!\n Tu ganas!");
 		else ST7735_OutString("Your character\n is very full\n on boba!\n You win!");
 	}
 	else{
-		if(language) ST7735_OutString("Tu personaje\n esta muy\n triste.\n \xADT\xA3 pierdes!");
+		if(language) ST7735_OutString("Tu personaje\n esta muy\n triste.\n Tu pierdes!");
 		else ST7735_OutString("Your character is\n  sad.\n  He dropped out of \n  his major.");
 	}
 }
